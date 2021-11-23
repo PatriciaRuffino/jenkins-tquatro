fc03e0da8a44e61d9ebc235c7937f0d461cfc8df
Patricia Ruffino <sindelformr@gmail.com>
2021-11-23 14:26:38 -0300

    criando projeto

---
+++ src/main/java/dev/ruffino/jenkinstquatro/JenkinsTquatroApplication.java
@@ -0,0 +1,13 @@
+package dev.ruffino.jenkinstquatro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTquatroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTquatroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ruffino/jenkinstquatro/JenkinsTquatroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ruffino.jenkinstquatro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTquatroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
